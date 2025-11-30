a84b1d3bb2bcec543e36c401d4ec9f2322771400
pH-Valiu <sdswae374@gmail.com>
2025-11-30 12:08:33 +0100

    We found out that inside the TESTs of NIST when it says KEY = 'feffe9928665731c6d6a8f9467308308', that it will be stored in memory like key[0] = fe, key[1] = ff, key[2]= e9, ... and not in LE specification, its a byte array and not a number

--- gcm/gcm.h
+++ gcm/gcm.h
@@ -2,8 +2,6 @@
 #define GCM_H
 
 #include "wmmintrin.h"
-#include "immintrin.h"  // for _xgetbv
-#include "qglobal.h"    // for Q_OS_X macros
 #include <QString>
 #include <QByteArray>
 
@@ -35,6 +33,7 @@ QByteArray decrypt(const QByteArray& key, const QByteArray& iv, const QByteArray
 bool authenticate(const QByteArray& aad, const QByteArray& c, const QByteArray& t);
 bool authenticate(const QByteArray& aad, const struct GCM_OUT& gcm_out);
 QByteArray random_iv();
+void gcm_test();
 
 void singleAESBlock(const unsigned char* in, unsigned char* out, unsigned char length, const unsigned char* key, int number_of_rounds);
 
@@ -44,7 +43,7 @@ void singleAESBlock(const unsigned char* in, unsigned char* out, unsigned char l
 static constexpr quint8 gcm_keyLength = 16;
 static constexpr quint8 gcm_keyLengthBits = 128;
 static constexpr quint8 gcm_expKeyLength = 176;
-static constexpr quint8 gcm_nRounds = 10;
+static constexpr quint8 gcm_n_rounds = 10;
 
 // ----------------------------------
 // AES-GCM data lengths requirements
@@ -53,43 +52,11 @@ static constexpr quint64 MAX_PLAIN_LEN  = 549755813632ULL;          // 2^39 - 25
 static constexpr quint64 MAX_AAD_LEN    = 18446744073709551615ULL;  // 2^64 - 1
 static constexpr quint64 MAX_IV_LEN     = 18446744073709551615ULL;  // 2^64 - 1
 
+// ----------------------------
+// Further constant values
+// ----------------------------
+static __m128i BSWAP_MASK = _mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+static __m128i BSWAP_EPI64_MASK = _mm_set_epi8(8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5, 6, 7);
 
-
-// -------------------------------
-// OS and CPU architecture checks
-// -------------------------------
-
-bool osSupportAVX() {
-    unsigned long long xcr0 = _xgetbv(0);
-    return (xcr0 & 0x6) == 0x6;      // OS AVX2 support
-}
-#ifdef Q_OS_WIN
-#include <intrin.h>
-static void cpuid(int out[], int x){
-    __cpuidex(out, x, 0);
-}
-bool hasAES() {
-    int info[4];
-    cpuid(info, 1);
-    return info[2] & 0x2000000 ;  // AES-NI
-}
-bool hasPCLMUL() {
-    int info[4];
-    cpuid(info, 1);
-    return info[2] & 0x2;   // PCLMULQDQ
-}
-bool hasAVX2() {
-    int info[4];
-    cpuid(info, 7);
-    return (info[1] & 0x20) && osSupportAVX();   // AVX2 bit
-}
-
-#elif defined(Q_OS_LINUX)
-bool hasAES()       { return __builtin_cpu_supports("aes"); }
-bool hasPCLMUL()    { return __builtin_cpu_supports("pclmul"); }
-bool hasAVX2()      { return __builtin_cpu_supports("avx2") && osSupportAVX(); }
-#else
-#error "We do not support that OS version yet
-#endif
 
 #endif // GCM_H
